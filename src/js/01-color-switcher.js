4abb71cf2ac8cdea68046c7dc0542ff3db7c32dd
Tetiana <tania.pokhylko@gmail.com>
2023-06-03 23:54:13 +0300

    hw-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startButtonEl = document.querySelector('button[data-start]');
+const stopButtonEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+stopButtonEl.disabled = true;
+let intervalID;
+
+startButtonEl.addEventListener('click', onStartClick);
+stopButtonEl.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  intervalID = setInterval(() => {
+    const color = getRandomHexColor();
+    bodyEl.style.backgroundColor = `${color}`;
+  }, 1000);
+  startButtonEl.disabled = true;
+  stopButtonEl.disabled = false;
+}
+
+function onStopClick() {
+  clearInterval(intervalID);
+  startButtonEl.disabled = false;
+  stopButtonEl.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
